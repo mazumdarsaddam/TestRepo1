4a4760ae7f58e822e1a64851a723947ec740fe12
jamshed <119809089+mazumdarsaddam@users.noreply.github.com>
2023-03-04 10:24:15 -0800

    Update ArrayDemo.java
    //Additional optional comments

--- JavaFeb23/src/Array/ArrayDemo.java
+++ JavaFeb23/src/Array/ArrayDemo.java
@@ -4,6 +4,8 @@ public class ArrayDemo {
 	public static void main(String[] args) {
 		
 		// declaring Array
+		//this is a new dimonsanal
+		
 		String[] myArray = new String [3];
 		myArray[0] = "Selenium";
 		myArray[1] = "Eclipse" ;
